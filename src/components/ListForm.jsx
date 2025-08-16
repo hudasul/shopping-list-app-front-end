219cbc2ccfff10c8b554ca977153d0a05775ea86
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-08-16 17:27:39 +0300

    show list immediatilly after it is added

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -3,18 +3,16 @@ import { useState } from "react";
 import axios from "axios";
 
 const ListForm = (props) => {
-
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    date: ''
-  })
+    name: "",
+    date: "",
+  });
 
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
-  
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -25,27 +23,38 @@ const ListForm = (props) => {
     const response = await axios.post(url, formData);
 
     if (response.status === 200 || response.status === 201) {
+      props.setShoppingLists([...props.shoppingLists, response.data]);
       props.setFormIsShown(false);
-     props.setAddList(false);
+      props.setAddList(false);
     }
 
     setIsSubmitted(false);
-
-
-  }
+  };
 
   return (
     <>
       <h1>Add a new Shopping List</h1>
 
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
         <br />
         <br />
 
         <label htmlFor="date">Last date to purchase: </label>
-        <input type="date" id="date" name="date" value={formData.date} onChange={handleChange}/>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={formData.date}
+          onChange={handleChange}
+        />
         <br />
         <br />
 
