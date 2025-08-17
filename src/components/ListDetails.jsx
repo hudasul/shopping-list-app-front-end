19751ca7105cced7fcce58d2ca2b1fb842b9d46d
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-08-17 09:45:20 +0300

    add item to a shopping list

--- src/components/ListDetails.jsx
+++ src/components/ListDetails.jsx
@@ -1,50 +1,43 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
+const ListDetails = (props) => {
+  const [listItems, setListItems] = useState([]);
+  const List = props.selectedList;
+  const getShoppingListItems = async () => {
+    const url = `${import.meta.env.VITE_BACKEND_URL}/shoppingList/${
+      List._id
+    }/item`;
+    const response = await axios.get(url);
+    setListItems(response.data);
+  };
 
-const  ListDetails =(props)=> {
-
-    const [listItems, setListItems] = useState([])
-    const List = props.selectedList
-    const getShoppingListItems = async ()=>{
-       const url = `${import.meta.env.VITE_BACKEND_URL}/shoppingList/${List._id}/item`
-       const response = await axios.get(url)
-       setListItems(response.data)
-    }
-
-    useEffect(() => {
-        getShoppingListItems()
-      }, []);
+  useEffect(() => {
+    getShoppingListItems();
+  }, []);
 
   return (
     <>
-       <h1>{List.name}</h1> 
-       {
-       
-       listItems
-       ?
-       listItems.map((item)=>{
-        return(
+      <h1>{List.name}</h1>
+      <button onClick={props.handleAddItemButtonClick}>Add Item</button>
+      {listItems ? (
+        listItems.map((item) => {
+          return (
             <div key={item._id}>
-                <h2>{item.name}</h2>
-                <p>Price: {item.price}</p>
-                <p>Quantity: {item.quantity}</p>
-                <button>Update</button>
-                <button>Delete</button>
+              <h2>{item.name}</h2>
+              <p>Price: {item.price}</p>
+              <p>Quantity: {item.quantity}</p>
+              <button>Update</button>
+              <button>Delete</button>
             </div>
-        )
-       })
-       :
-       <h2>There is no items in this list</h2>
-
-       
-       }
-       
-
-       
+          );
+        })
+      ) : (
+        <h2>There is no items in this list</h2>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ListDetails
+export default ListDetails;
