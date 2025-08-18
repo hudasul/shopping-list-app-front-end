f5b338994ae40130139f858796d2d5601b581fcb
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-08-18 11:14:51 +0300

    did the authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,67 @@ import AllLists from "./components/AllLists";
 import ListForm from "./components/ListForm";
 import ListDetails from "./components/ListDetails";
 import ItemForm from "./components/ItemForm";
+import LoginForm from "./components/LoginForm";
+import SignUp from "./components/SignupForm";
+import LogoutButton from "./components/LogoutButton";
+import ProtectedRoute from "./components/ProtectedRoute";
 
+import {jwtDecode} from 'jwt-decode'
+import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 
-const App = ()=> {
+const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  function handleLogin(newToken) {
+    setToken(newToken);
+  }
+
+  function handleLogout() {
+    setToken(null);
+    localStorage.removeItem("token");
+  }
+
+  // This is how to decode the token and gget the
+  // information that you added to the payload in your login
+  // route in the backend
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    console.log(decodedToken);
+  }
+
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<AllLists />} />
-        <Route path="/new-list" element={<ListForm />} />
-        <Route path="/edit-list/:listId" element={<ListForm />} />
-        <Route path="/list/:listId" element={<ListDetails />} />
-        <Route path="/list/:listId/new-item" element={<ItemForm />} />
-        <Route path="/list/:listId/edit-item/:itemId" element={<ItemForm />} />
-      </Routes>
-    </Router>
+      <Router>
+        <div>
+          {token ? <LogoutButton onLogout={handleLogout} /> : null}
+          <Routes>
+            <Route
+              path="/login"
+              element={<LoginForm onLogin={handleLogin} />}
+            />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <AllLists />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/new-list" element={<ListForm />} />
+            <Route path="/edit-list/:listId" element={<ListForm />} />
+            <Route path="/list/:listId" element={<ListDetails />} />
+            <Route path="/list/:listId/new-item" element={<ItemForm />} />
+            <Route
+              path="/list/:listId/edit-item/:itemId"
+              element={<ItemForm />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
--- src/components/AllLists.jsx
+++ src/components/AllLists.jsx
@@ -3,22 +3,28 @@ import { useNavigate } from "react-router";
 import axios from "axios";
 
 const AllLists = () => {
+  const [errors, setErrors] = useState("");
   const [lists, setLists] = useState([]);
   const baseUrl = import.meta.env.VITE_BACKEND_URL;
   const navigate = useNavigate();
 
-   const getAllLists = async () => {
+  const getAllLists = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
     try {
       const url = `${baseUrl}/shoppingList`;
-      const response = await axios.get(url);
+      const response = await axios.get(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
       setLists(response.data);
     } catch (error) {
-      console.log(error);
+      setErrors("Unauthorized or error fetching data");
     }
-  }
+  };
 
   useEffect(() => {
-   getAllLists()
+    getAllLists();
   }, []);
 
   const handleDelete = async (listId) => {
@@ -26,21 +32,28 @@ const AllLists = () => {
     setLists((prev) => prev.filter((list) => list._id !== listId));
   };
 
+  if (!lists) return <p>No data yet</p>;
   return (
     <>
-    <div>
-      <h2>All Shopping Lists</h2>
-      <button onClick={() => navigate("/new-list")}>Add New List</button>
-      {lists.map((list) => (
-        <div key={list._id}>
-          <h1>{list.name}</h1>
-          <p>Last date to purchase: {new Date(list.date).toLocaleDateString()}</p>
-          <button onClick={() => navigate(`/list/${list._id}`)}>View</button>
-          <button onClick={() => navigate(`/edit-list/${list._id}`)}>Edit</button>
-          <button onClick={() => handleDelete(list._id)}>Delete</button>
-        </div>
-      ))}
-    </div>
+      <div>
+        <h2>All Shopping Lists</h2>
+        <button onClick={() => navigate("/new-list")}>Add New List</button>
+        {lists.map((list) => (
+          <div key={list._id}>
+            <h1>{list.name}</h1>
+            <p>
+              Last date to purchase: {new Date(list.date).toLocaleDateString()}
+            </p>
+            <button onClick={() => navigate(`/list/${list._id}`)}>View</button>
+            <button onClick={() => navigate(`/edit-list/${list._id}`)}>
+              Edit
+            </button>
+            <button onClick={() => handleDelete(list._id)}>Delete</button>
+          </div>
+        ))}
+
+        <p style={{ color: "darkred" }}>{errors}</p>
+      </div>
     </>
   );
 };
