05179b4d4bac1e3a316e6050b0e9d8df7f5ea3d7
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-08-19 15:57:38 +0300

    designed the signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,53 +1,60 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router'
-import axios from 'axios'
-import './style/LoginForm.css'
-function SignUp() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const navigate = useNavigate()
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
-  const handleSubmit = async event => {
-    event.preventDefault()
+import "./style/SignupForm.css";
+function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-      await axios.post('http://localhost:3000/auth/signup', {
+      await axios.post("http://localhost:3000/auth/signup", {
         username,
-        password
-      })
-      navigate('/login')
+        password,
+      });
+      navigate("/login");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
     <body>
-    <div className='container'>
-    <form onSubmit={handleSubmit}>
-      <h1>Sign Up</h1>
-      <input 
-        placeholder="Username"
-        value={username}
-        onChange={event => setUsername(event.target.value)}
-      />
-      <br />
-      <br />
-      <input 
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={event => setPassword(event.target.value)}
-      />
-      <br />
-      <br />
-      <button type="submit">Sign Up</button>
-      <br />
-      <br />
-      <button type="submit" onClick={()=> navigate('/login')}>Login</button>
-  
-    </form>
-    </div>
-     </body>
-  )
+      <div className="signup-container">
+        <form onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
+          <div className="Signup-inputs">
+            <input
+              placeholder="Username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            <br />
+            <br />
+            <input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <br />
+          <br />
+
+          <div className="signup-btns">
+            <button type="submit">Sign Up</button>
+            <br />
+            <br />
+            <button type="submit" onClick={() => navigate("/login")}>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </body>
+  );
 }
-export default  SignUp
+export default SignUp;
